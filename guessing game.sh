3ca9b0005d735b98a90031690831cb13b2bf4a64
Devaashish Sharma <devaashishsharma402@gmail.com>
2019-03-12 17:26:48 +0530

    Create guessing game.sh

---
+++ guessing game.sh
@@ -0,0 +1,26 @@
+!/usr/bin/env bash
+#File: guessinggame.sh
+#Created by: Devaashish Sharma
+
+
+actualcount=$(ls -1 | wc -l)
+
+function user_hint {        # function
+    if [[ $1 -gt $2 ]]          # if statement
+    then
+	echo "Too high guessed!"
+    elif [[ $1 -lt $2 ]]
+    then
+	echo "Too low guessed!"
+    else
+	echo ""
+	echo "Congratualations! nailed it!"
+    fi
+}
+
+while [[ $actualcount -ne $guess ]]    # loop
+do
+    read -p "Guess how many files are in the current directory? " guess
+    echo $(user_hint $guess $actualcount)
+    echo ""
+done
